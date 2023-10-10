62e6decd6c7fcd701bf55b0dc8f4c27b9d485ae3
Firdous91 <147132808+Firdous91@users.noreply.github.com>
2023-10-10 20:57:47 +0530

    Add files via upload

---
+++ 22wh1a0582-class/Details.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class Details{
+       public static void main(string[] args) throus IOException{
+                InputStreamReader isr=new InputStreamReader(System.in);
+	        BufferedReader br= new BufferedReader(isr);
+		System.out.println("enter age:");
+		string b1=br.readLine();
+		int age =Integer.parseInt(s1);
+		System.out.println("enter gender:");
+		char g=(char)br.read().charAt(0);
+		System.out.println("enter name:");
+		string name=br.readLine();
+		{
+			System.out.println("age:"+b1);
+			System.out.println("gender:"+g);
+			System.out.println("name:"+name);
+		}
+}
+}	
