34d56a00cf699fb09edb4b739b696f52fcecb940
Sirinakka05c6 <147131897+Sirinakka05c6@users.noreply.github.com>
2023-10-10 20:55:31 +0530

    Add files via upload

---
+++ 22wh1a05c6-Class/SumOfDigits.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class SumOfDigits { 
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println("The sum of the digits of " + n +" is: " + sumDigits(n));
+
+    }
+
+    public static int sumDigits(long n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
