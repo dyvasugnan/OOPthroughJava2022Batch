db418610a6a06d27592c2aa5d1cc094ddd30fbb7
Neeharika577 <147133078+Neeharika577@users.noreply.github.com>
2023-10-11 19:55:22 +0530

    added Box2.java

---
+++ 22wh1a0577-lab/Box2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Box2{
+    double l,w,h;
+
+    Box2(double a,double b,double c){
+        l=a;
+        h=b;
+        w=c;
+    }
+    double volume(){
+        return l*w*h;}
+
+
+    public static void main(String args[]){
+        Box2 b1=new Box2(1,10,10);
+        System.out.println(b1.volume());
+        Box2 b2=new Box2(2.5,2.0,2.0);
+        System.out.println(b2.volume());
+    }}
