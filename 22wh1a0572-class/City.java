70960e692007b5c67859cef8e270cb38ff9f041e
PagadalaNikitha572 <147132859+PagadalaNikitha572@users.noreply.github.com>
2023-10-11 20:48:30 +0530

    Add files via upload

---
+++ 22wh1a0572-class/City.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class City {
+    public static void main(String[] args) {
+        BufferedReader  br=new BufferedReader(new InputStreamReader(System.in);
+	String[] cities = new String[10];
+        System.out.println("Enter cities: ");
+
+        for (i=0;i<10;i++){
+            cities[i] =br.readline();
+        }
+        System.out.print("Enter key");
+        String key = br.readLine();
+        for(i=0;i<10;i++) {
+            if(cities[i]==key){
+              search=i;
+              break;
+            }}
+        
+        System.out.println("Key found at:"+search);
+        }}
