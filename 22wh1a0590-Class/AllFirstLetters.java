5f6b7043157ab0fbc6324a1d47d9042970472366
deekshitha590 <130597241+deekshitha590@users.noreply.github.com>
2023-10-12 21:36:53 +0530

    Add files via upload

---
+++ 22wh1a0590-Class/AllFirstLetters.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+public class AllFirstLetters{
+   public static void main(String[] args)throws Exception{
+   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+   System.out.print("Enter text with spaces:");
+   String str = br.readLine();
+   StringTokenizer st = new StringTokenizer(str ,"  ");
+   String s1 = st.nextToken();
+   String s2 = st.nextToken();
+   String s3 = st.nextToken();
+   String s4 = st.nextToken();
+   System.out.print(s1.substring(0, 1).toUpperCase() + s1.substring(1)+ " ");
+   System.out.print(s2.substring(0, 1).toUpperCase() + s2.substring(1)+ " ");
+   System.out.print(s3.substring(0, 1).toUpperCase() + s3.substring(1)+ " ");
+   System.out.print(s4.substring(0, 1).toUpperCase() + s4.substring(1)+ " ");
+
+
+}
+}
+
+
