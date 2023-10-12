dba90f9337038d0b44c77ecf08046cdc7ab2070f
varshasric4 <147131871+varshasric4@users.noreply.github.com>
2023-10-12 21:30:47 +0530

    Add files via upload

---
+++ 22WH1A05B8-class/CombineFirst.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class CombineFirst{
+    public static void main(String[] args)throws IOException{
+        InputStreamReader isr=new InputStreamReader(System.in);
+        BufferedReader br=new BufferedReader(isr);
+        String str=br.readLine();
+        String s[]=new String[10];
+        s=str.split(" ");
+        for(int i=0;i<s.length;i++){
+            String word=s[i];
+            System.out.print(Character.toString(word.charAt(0)));
+        }
+        
+     }
+}
