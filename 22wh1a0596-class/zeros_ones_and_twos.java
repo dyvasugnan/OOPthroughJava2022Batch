41234ce78df149e8dcaf1aab39beca8f5192c580
Pranavi596 <147131832+Pranavi596@users.noreply.github.com>
2023-10-12 21:07:11 +0530

    Add files via upload

---
+++ 22wh1a0596-class/zeros_ones_and_twos.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Sorting{
+    public static void main(String[] args){
+        int arr[] = {0,1,0,0,2,1};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
