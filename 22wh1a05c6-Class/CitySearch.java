9ca8a6466f1e8a143d57e06dfacba221b424c6b1
Sirinakka05c6 <147131897+Sirinakka05c6@users.noreply.github.com>
2023-10-12 23:20:53 +0530

    Add files via upload

---
+++ 22wh1a05c6-Class/CitySearch.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+public class CitySearch {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the number of cities: ");
+        String s = br.readLine();
+        int n = Integer.parseInt(s);
+        String[] cities = new String[n];
+
+        System.out.println("Enter cities separated by spaces: ");
+        StringTokenizer allCities = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+                cities[i] = allCities.nextToken().trim();
+        }
+
+        System.out.println("Enter the city to be searched: ");
+        String search = br.readLine().trim();
+        int cityIndex = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (cities[i].equals(search)) {
+                cityIndex = i+1;
+                break;
+            }
+            
+        }
+
+        if (cityIndex == 0) {
+            System.out.println("City not found.");
+        } else {
+            System.out.println("City is found at "  + " position "+ cityIndex);
+        }
+    }
+}
