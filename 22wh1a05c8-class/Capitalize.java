f8f704c4cdc62b40af6c11f6b2d89c4eaf6228f0
PolapalliVennela <147133276+PolapalliVennela@users.noreply.github.com>
2023-10-13 00:00:01 +0530

    Add files via upload

---
+++ 22wh1a05c8-class/Capitalize.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Capitalize {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String input = scanner.nextLine();
+        String[] words = input.split(" ");
+        StringBuilder capitalizedSentence = new StringBuilder();
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                char firstLetter = Character.toUpperCase(word.charAt(0));
+                String restOfWord = word.substring(1).toLowerCase();
+                capitalizedSentence.append(firstLetter).append(restOfWord).append(" ");
+            }
+        }
+        System.out.println("Capitalized Sentence: " + capitalizedSentence.toString().trim());
+    }
+}
