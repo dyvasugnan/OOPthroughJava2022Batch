ca595e6d5f47861065ba9ce27de16e6b23d6f25b
saividhya67 <147132744+saividhya67@users.noreply.github.com>
2023-10-12 22:46:46 +0530

    Add files via upload

---
+++ 22wh1a0567-class/Fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms in the Fibonacci: ");
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
