c46e8d9766054dc79e4072e61b77d195bf07fdb4
deekshitha590 <130597241+deekshitha590@users.noreply.github.com>
2023-10-12 20:48:22 +0530

    Add files via upload

---
+++ 22wh1a0590-Class/AddingStartingLetters.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class AddingStartingLetters{
+	public static void main(String[] args)throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s=br.readLine();
+		StringTokenizer words=new StringTokenizer(s," ");
+		int n=words.countTokens();
+		s =" ";
+		for(int i=0;i<n;i++){
+			s+=words.nextToken().charAt(0);
+		}
+		System.out.println("string with first letters is:"+s);
+	}
+}
+
