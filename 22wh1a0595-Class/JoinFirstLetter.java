7c1cef9c2ec6a9a8605593e9d2474b0e450570f2
Bhavika22595 <147132195+Bhavika22595@users.noreply.github.com>
2023-10-13 08:57:55 +0530

    Add files via upload

---
+++ 22wh1a0595-Class/JoinFirstLetter.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class JoinFirstLetter {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter a sentence: ");
+        String sentence = br.readLine();
+        StringBuilder firstLetters = new StringBuilder();
+        String[] words = sentence.split(" ");
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                firstLetters.append(word.charAt(0));
+            }
+        }
+
+        System.out.println("First Letters: " + firstLetters.toString());
+    }
+}
