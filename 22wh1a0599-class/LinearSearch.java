57e8f9f1d3292b6e7bcbd5e633d8efca41ced46a
ChHarshitha599 <147132208+ChHarshitha599@users.noreply.github.com>
2023-10-13 09:20:30 +0530

    Update LinearSearch.java

--- 22wh1a0599-class/LinearSearch.java
+++ 22wh1a0599-class/LinearSearch.java
@@ -2,16 +2,16 @@ import java.util.*;
 class LinearSearch {
 	public static void main(String[] args){
 		Scanner sc= new Scanner(System.in);
-		String cities[]=new String[2];
+		String cities[]=new String[10];
 		int temp=0;
 		System.out.println("Enter the city names :");
-		for(int i=0;i<2;i++)
+		for(int i=0;i<10;i++)
 		{
 			cities[i]=sc.next();
 		}
 		System.out.println("Enter the city to be searched :");
 		String searching_city=sc.next();
-		for(int i=0;i<2;i++)
+		for(int i=0;i<10;i++)
 		{
 			if(cities[i].equals(searching_city))
 			{
