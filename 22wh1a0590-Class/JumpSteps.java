68c436498cdad88f8ba9c6585585e744b147b048
deekshitha590 <130597241+deekshitha590@users.noreply.github.com>
2023-10-13 08:07:50 +0530

    Add files via upload

---
+++ 22wh1a0590-Class/JumpSteps.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class JumpSteps{
+public static void main(String[] args)throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("enter no of steps to climb");
+    String s = br.readLine();
+    int n = Integer.parseInt(s);
+    int a = 0;
+    int b = 1;
+    System.out.println(a);
+    System.out.println(b);
+    for(int i = 3; i <= n;i++){
+    int c = a + b;
+    System.out.println(c);
+    a = b;
+    b = c;
+}
+}
+}
+        
