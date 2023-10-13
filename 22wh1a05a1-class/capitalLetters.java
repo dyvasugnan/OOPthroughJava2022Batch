fefa61082c1e6c94cf52893be61476384051cfbc
Sanandha0907 <147132412+Sanandha0907@users.noreply.github.com>
2023-10-13 07:10:13 +0530

    Add files via upload

---
+++ 22wh1a05a1-class/capitalLetters.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class capitalLetters{
+   public static void main(String[] args)throws Exception{
+   InputStreamReader isr = new InputStreamReader(System.in);
+BufferedReader br = new BufferedReader(isr);
+   System.out.print("Enter text with spaces:");
+   String str = br.readLine();
+   StringTokenizer st = new StringTokenizer(str ,"  ");
+   String s1 = st.nextToken();
+   String s2 = st.nextToken();
+   String s3 = st.nextToken();
+   String s4 = st.nextToken();
+   System.out.print(s1.substring(0, 1).toUpperCase() + s1.substring(1)+ " ");
+   System.out.print(s2.substring(0, 1).toUpperCase() + s2.substring(1)+ " ");
+   System.out.print(s3.substring(0, 1).toUpperCase() + s3.substring(1)+ " ");
+   System.out.print(s4.substring(0, 1).toUpperCase() + s4.substring(1)+ " ");
+
+
+}
+}
