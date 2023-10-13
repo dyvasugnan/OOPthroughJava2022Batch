5a39c43bccb681c434b5934fc61a216bfcf13ede
deekshitha590 <130597241+deekshitha590@users.noreply.github.com>
2023-10-13 09:29:25 +0530

    Add files via upload

---
+++ 22wh1a0590-Lab/Area.java
@@ -0,0 +1,24 @@
+class Area{
+   void area(int l,int b){
+      System.out.println("area of rectangle is:" +(l*b));
+   }
+   void area(int l){
+      System.out.println("area of square is:" +(l*l));
+   }
+   void area(float l,float b){
+      System.out.println("area is:" +(l*b));
+   }
+   void area(int l,float b){
+      System.out.println("area is:" +(l*b));
+   }
+}
+class Demo{
+   public static void main(string[] args){
+      Area a = new Area();
+         a.area(2,3);
+         a.area(2);
+         a.area(2.3f,1.2f);
+         a.area(2,1.2f);
+   }
+}
+       
