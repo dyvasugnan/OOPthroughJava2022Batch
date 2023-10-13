7965d068832883eaa6d7de62546369944ff57de5
Pranavi596 <147131832+Pranavi596@users.noreply.github.com>
2023-10-13 09:19:42 +0530

    Add files via upload

---
+++ 22wh1a0596-Lab/opperations.java
@@ -0,0 +1,10 @@
+public class opperations{
+	public static void main(String[] args){
+		int a = 10,b = 20;
+		int c = a + b,d = b - a,x = a * b,y = a / b;
+		System.out.println("Sum:" + c);
+		System.out.println("Diff:" + d);
+		System.out.println("product:" + x);
+		System.out.println("div:" + y);
+	}
+}
