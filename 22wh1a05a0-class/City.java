4e110bc24ba18bfffe4f4a843ae29eedb8b807bf
amulya5a0 <147133539+amulya5a0@users.noreply.github.com>
2023-10-14 23:42:33 +0530

    Add files via upload

---
+++ 22wh1a05a0-class/City.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class City {
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		String cities[]=new String[10];
+		int temp=0;
+		System.out.println("Enter the names of the cities ");
+		for(int i=0;i<5;i++)
+		{
+			cities[i]=s.next();
+		}
+		System.out.println("Enter the city to be searched :");
+		String search_city=s.next();
+		for(int i=0;i<5;i++)
+		{
+			if(cities[i].equals(search_city))
+			{
+				temp=1;
+				break;
+			}
+
+		}
+		if (temp==1)
+		{
+			System.out.println("found");
+		}
+	}
+}
