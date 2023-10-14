50ad3b7839d0cb18f0fed4b7fc4afb51f4c8e28e
deekshitha590 <130597241+deekshitha590@users.noreply.github.com>
2023-10-14 23:27:14 +0530

    Add files via upload

---
+++ 22wh1a0590-Lab/BankDemo.java
@@ -0,0 +1,42 @@
+//package java_cseb;
+import java.util.*;
+
+class BankAcc{
+	double bal;
+	BankAcc(double b){
+		bal=b;
+	}
+	private class Interest{
+		double rate;
+		int time;
+		Interest(double r,int t){
+			rate=r;
+			time=t;
+		}
+		void show() {
+			System.out.println((bal*rate*time)/100);
+		}
+	}
+	void contact(double r,int time) {
+		System.out.println("Enter the password :");
+		Scanner sc=new Scanner(System.in);
+		String p=sc.next();
+		if(p.equals("1234")) {
+			Interest in=new Interest(r,time);
+			in.show();
+		}
+		else {
+			System.out.println("Wrong Password !");
+		}
+	}
+}
+public class BankDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BankAcc bac=new BankAcc(5000.0);
+		bac.contact(5.0,8);
+		
+	}
+
+}
