a245aaec6151e70adfbd852a2124b00e0058df61
23WH5A0510 <147133362+23WH5A0510@users.noreply.github.com>
2023-10-16 23:37:10 +0530

    23wh5a0510.lab

---
+++ Search.java
@@ -0,0 +1,25 @@
+import  java.io.*;
+import  java.util.*;
+class  Search
+{
+public static void main(String[] args)  throws IOException
+{
+int i;
+InputStreamReader isr=new InputStreamReader(System.in);
+BufferedReader br=new BufferedReader(isr);
+String s[]=new String[5];
+System.out.println("enter the cities");
+for(i=0;i<5;i++)
+{
+s[i]=br.readLine();
+}
+System.out.println("enter the  element to find");
+String  ele=br.readLine();
+for(i=0;i<5;i++)
+{
+if(s[i]==ele)
+break;
+}
+System.out.println("element found  "+ele);
+}
+}
