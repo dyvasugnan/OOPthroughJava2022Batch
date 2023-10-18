efed1f116acfa7d1dfcd7da07f706535d2ccf718
saividhya67 <147132744+saividhya67@users.noreply.github.com>
2023-10-18 22:43:38 +0530

    Add files via upload

---
+++ 22wh1a0567-Lab/TwoString.java
@@ -0,0 +1,16 @@
+package cse22567;
+import java.util.*;
+
+
+public class TwoString {
+
+	public static void main(String[] args) {
+		String s1 = "bvrit";
+		String s2 = "hyd";
+		String s3=s1.concat(s2);
+		
+		System.out.println("s3:"+ s3);
+
+	}
+
+}
