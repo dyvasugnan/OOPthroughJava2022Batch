a51c6cc7a8b1fabe5a809fbfd77b0a80477805d3
22wh1a05a8 <147132110+22wh1a05a8@users.noreply.github.com>
2023-10-22 11:48:05 +0530

    Update RandomNumberGame.java

--- 22wh1a05a8-class/RandomNumberGame.java
+++ 22wh1a05a8-class/RandomNumberGame.java
@@ -1,37 +1,41 @@
 import java.util.Scanner;
-public class RandomNumberGame{
-	public static void main(String args[])
-	{
-	    int win = 0;
-	    System.out.println("Select any of the following options");
-	    System.out.println("1 : Play the game");
-	    System.out.println("2 : Show the score");
-	    System.out.println("3 : End the game");
-	    Scanner sc = new Scanner(System.in);
-	    int ch = sc.nextInt();
-	    int ran = (int) Math.round(Math.random() * (10));
-	    switch (ch) {
-          case 1:
-            System.out.println("You have only three choices to guess the number");
-		    for(int i=0;i<3;i++){
-		        System.out.println("Enter a number");
-		        int n = sc.nextInt();
-		        if(n==ran){
-		            System.out.println("You guessed it right!!!");
-		            win++;
-		            break;
-		        }
-		        else
-		            System.out.println("You failed to guess");
-		    }
-		    System.out.println("The number is "+ran);
-            break;
-          case 2:
-            System.out.println("Your score is "+(win*10));
-            break;
-          case 3:
-            System.out.println("ENDING THE GAME!");
-            break;
+public class RandomNumberGame {
+    public static void main(String args[]) {
+        int win = 0;
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Select any of the following options");
+            System.out.println("1 : Play the game");
+            System.out.println("2 : Show the score");
+            System.out.println("3 : End the game");
+            int ch = sc.nextInt();
+            int ran = (int) Math.round(Math.random() * 10);
+            switch (ch) {
+                case 1:
+                    System.out.println("You have only three choices to guess the number");
+                    for (int i = 0; i < 3; i++) {
+                        System.out.println("Enter a number");
+                        int n = sc.nextInt();
+                        if (n == ran) {
+                            System.out.println("You guessed it right!!!");
+                            win++;
+                            break;
+                        } else
+                            System.out.println("You failed to guess");
+                    }
+                    System.out.println("The number is " + ran);
+                    break;
+                case 2:
+                    System.out.println("Your score is " + (win * 10));
+                    break;
+                case 3:
+                    System.out.println("ENDING THE GAME!");
+                    sc.close();
+                    return;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
         }
-	}
+    }
 }
