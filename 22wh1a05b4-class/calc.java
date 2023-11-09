e58a36ea4d1d51a09b89b91d9c5f40f8898b83c4
22wh1a05b4 <147132223+22wh1a05b4@users.noreply.github.com>
2023-11-09 20:20:23 +0300

    Add files via upload

---
+++ 22wh1a05b4-class/calc.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class OneD{
+
+    int arr1[]=new int[10];
+
+
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+
+    int i;
+
+    void read()
+
+    {
+        
+
+      System.out.print("enter elements of array1:");
+
+      for(i=0;i<n;i++)
+      {
+
+        arr1[i]=sc.nextInt();
+
+      }
+
+    }
+
+    void print()
+ 
+    {
+
+      System.out.print("elements of array1:");
+
+    
+      for( i=0;i<n;i++){
+        
+        System.out.println(arr1[i]);
+    
+      }
+    
+    }
+
+}
+
+class TwoD{
+  
+   
+
+}
+
+class calc {
+    
+public static void main(String[] args) {
+        
+   OneD a=new OneD();
+       
+       
+       
+   a.read();
+        
+   a.print();
+    
+  }
+
+}
