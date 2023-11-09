0178618e8342b5f3f3559e90b070504e424a0092
sivapriya568 <147132673+sivapriya568@users.noreply.github.com>
2023-11-09 19:27:44 +0530

    Update SuperArray.java

--- 22wh1a0568-class/SuperArray.java
+++ 22wh1a0568-class/SuperArray.java
@@ -1,8 +1,16 @@
 package LearningJava;
+import java.util.Scanner;
 class OneD {
 	int[] arr1D;
 	OneD() {
-        arr1D = new int[] {1, 2, 3}; 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter array size:");
+		int size = sc.nextInt();
+        arr1D = new int[size];
+        System.out.println("Enter the elements:");
+        for(int i =0 ;i<size;i++) {
+        	arr1D[i] = sc.nextInt();
+        }
         System.out.println("1D array in Superclass: ");
         for (int i = 0; i < arr1D.length; i++) {
             System.out.print(arr1D[i] + " ");
@@ -13,19 +21,26 @@ class OneD {
 }
 
 class TwoD extends OneD {
-    int[] newColumns;
+    int[] newData;
     int[][] arr2D;
     TwoD() {
-        newColumns = new int[] {4, 5, 6};
-        int term;
-        /*if(arr1D.length==2)
+    	Scanner sc = new Scanner(System.in);
+    	System.out.print("Enter column size:");
+		int cols = sc.nextInt();
+		newData = new int[cols];
+        System.out.println("Enter the elements:");
+        for(int i =0 ;i<cols;i++) {
+        	newData[i] = sc.nextInt();
+        }
+        /*int term;
+        if(arr1D.length==2)
         	term =0;
         else
         	term = arr1D.length/2;
         int rows = arr1D.length-term; // no.of rows 
         int cols = newColumns.length; // no.of columns */
-	int rows =2;
-	int cols = 3;
+        int rows = 2;
+        //int cols = 3;
         int[][] arr2D = new int[rows][cols];
 
         for (int i = 0; i < rows; i++) {
@@ -33,7 +48,7 @@ class TwoD extends OneD {
                 if (i == 0) {
                     arr2D[i][j] = arr1D[j];
                 } else {
-                    arr2D[i][j] = newColumns[j];
+                    arr2D[i][j] = newData[j];
                 }
             }
         }
@@ -45,10 +60,12 @@ class TwoD extends OneD {
             System.out.println();
         }
     }
-}
+}		
+
 
 public class SuperArray {
     public static void main(String[] args) {
         TwoD sub = new TwoD();
     }
 }
+
