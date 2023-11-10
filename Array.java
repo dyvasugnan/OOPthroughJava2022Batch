72f1c69353ab23d2735c15b7ce7011b2c324fd00
Trivenikolli <147131912+Trivenikolli@users.noreply.github.com>
2023-11-10 08:08:39 +0530

    Add files via upload

--- Array.java
+++ Array.java
@@ -1,54 +1,53 @@
-package javaprog;
-import java.util.Scanner;
+import java.util.*;
 
+class OneD {
+    int arr_one[];
 
-class OneDArr {
-	int[] arr;
-    
-    void oneD() {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter size: ");
-        int size=sc.nextInt();
-        arr=new int[size];
-        System.out.println("Enter elements:");
-        for (int i=0; i<size;i++) {
-            arr[i]=sc.nextInt();
+    void arr_1() {
+        System.out.println("Enter the size");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        arr_one = new int[size]; 
+        for (int i = 0; i < size; i++) { 
+            arr_one[i] = sc.nextInt();
         }
-        System.out.println("The elements of the array are:");
-        for (int i=0;i<size;i++) {
-            System.out.print(arr[i] + " ");
+        System.out.println("OneD array is: ");
+        for (int i = 0; i < size; i++) { 
+            System.out.print(arr_one[i] + " "); 
         }
     }
 }
-class TwoD extends OneDArr{
-	void twoD(){
-		System.out.println("Enter number of rows:");
-		Scanner sc=new Scanner(System.in);
-		int rows=sc.nextInt();
-		System.out.println("Enter number of columns:");
-		int col=sc.nextInt();
-		int arr2[][]=new int[rows][col];
-		int index=0;
-		for (int i=0; i<rows;i++){
-			for(int j=0;j<col;j++){
-					if(index<arr.length){
-						arr2[i][j]=arr[index++];
-				}
-		  }
-		}
 
-		for(int j=0;j<col;j++){
-			for(int i=0;i<rows;i++){
-				System.out.print(arr2[i][j]+" ");
-			}
-			System.out.println();
-		}
-	}
+class TwoD extends OneD {
+    int arr_2[][];
+
+    void arr_2() {
+        System.out.println("Enter no of rows");
+        Scanner s = new Scanner(System.in);
+        int rows = s.nextInt();
+        System.out.println("Enter no of columns");
+        int columns = s.nextInt();
+        
+        arr_2 = new int[rows][columns]; 
+        
+        int index = 0; // Initialize index
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (index < arr_one.length) {
+                    arr_2[i][j] = arr_one[index++];
+                }
+            }
+        }
+        for(int i=0;i<rows;i++){
+           for(int j=0;j<columns;j++){
+             System.out.println(
 }
-public class Demo {
-    public static void main(String[] args) {
-        TwoD obj=new TwoD();
-        obj.oneD();
-        obj.twoD();    
+}
+
+public class Array{
+    public static void main(String[] args) { 
+        TwoD t = new TwoD(); 
+        t.arr_1();
+        t.arr_2();
     }
 }
