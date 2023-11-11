e88f42b84827dbd8873680308933ae26f33706d2
22wh1a05a8 <147132110+22wh1a05a8@users.noreply.github.com>
2023-11-11 21:01:31 +0530

    Create Matrix90.java

---
+++ 22wh1a05a8-class/Matrix90.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        int[][] matrix = new int[m][n];
+        int[][] matrix1 = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix1[j][m - i - 1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.printf(matrix1[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
