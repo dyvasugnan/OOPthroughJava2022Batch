577bc5674cc6fec672b5900089e50bdddaee8ddc
PETTEMCHARITHA <147132793+PETTEMCHARITHA@users.noreply.github.com>
2023-11-13 15:21:56 +0530

    Add files via upload

---
+++ 22wh1a05c1-lab/InnerDemo.java
@@ -0,0 +1,26 @@
+package cseb;
+
+class Outer{
+			void outershow() {
+						System.out.println("Outer method");
+						
+						class Inner{
+							void innershow() {
+								System.out.println("Inner method");
+			}
+		}//end of inner class
+		Inner in=new Inner();
+		in.innershow();
+	}
+}
+
+
+public class InnerDemo {
+
+	public static void main(String[] args) {
+	    Outer out=new Outer();
+		out.outershow();
+ 
+	}
+
+}
