5ebaa3a27801e601877bc40d44970881e6794ad3
Manvithach97 <147131977+Manvithach97@users.noreply.github.com>
2023-11-16 22:36:01 +0530

    Add files via upload

---
+++ 22wh1a0597-class/Matrix2D.java
@@ -0,0 +1,44 @@
+package hai;
+
+import java.util.Scanner;
+
+public class Matrix2D {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        System.out.print("Enter the number of columns: ");
+        int columns = scanner.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+
+        System.out.println("Enter the matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == 0) {
+                    for (int k = 0; k < columns; k++) {
+                        matrix[i][k] = 0;
+                    }
+                    break;
+                }
+            }
+        }
+    
+
+        System.out.println("Matrix:");
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
