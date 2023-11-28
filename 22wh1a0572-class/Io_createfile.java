fecc9a17aa552a06ed255f503ecbf376c75e820b
PagadalaNikitha572 <147132859+PagadalaNikitha572@users.noreply.github.com>
2023-11-28 19:38:56 +0530

    Update Io_createfile.java

--- 22wh1a0572-class/Io_createfile.java
+++ 22wh1a0572-class/Io_createfile.java
@@ -4,16 +4,15 @@ import java.io.IOException;
 
 public class Calci {
     public static void main(String[] args) {
-        // Specify the path to the file you want to read
         try (DataInputStream dis = new DataInputStream(System.in);
              FileOutputStream fos = new FileOutputStream("ntfile.txt")) {
-
-            char ch;
-            while ((ch = (char) dis.read()) != 'n') {
+             char ch;
+             while ((ch = (char) dis.read()) != 'n') {
                 fos.write(ch);
             }
 
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
     }
