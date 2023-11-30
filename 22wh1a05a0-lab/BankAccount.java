99e8db9b5d7bd5966e455cbc9830840184da5629
amulya5a0 <147133539+amulya5a0@users.noreply.github.com>
2023-11-30 15:19:15 +0530

    Add files via upload

---
+++ 22wh1a05a0-lab/BankAccount.java
@@ -0,0 +1,42 @@
+package cseb;
+import java.util.*;
+class BankAcc{
+	double bal;
+	BankAcc(double b){
+		bal = b;
+	}
+	void contact(double r) {
+		Scanner sc = new Scanner(System.in);
+		String pwd;
+		System.out.println("enter password");
+		pwd = sc.next();
+		if(pwd.equals("abc123")) {
+			Inter in = new Inter(r);
+			in.amount();
+		}
+		else {
+			System.out.println("invalid password");
+		}
+	}	
+		private class Inter{
+			double rate;
+			Inter(double r){
+			rate = r;
+			}
+			void amount() {
+				double amounts=(bal*2*rate)/100;
+				System.out.println("amount="+amounts);
+			}
+			
+			
+		}
+	
+}
+public class BankAccount {
+
+	public static void main(String[] args) {
+		BankAcc b = new BankAcc(1000);
+		b.contact(9.6);
+
+	}
+}
