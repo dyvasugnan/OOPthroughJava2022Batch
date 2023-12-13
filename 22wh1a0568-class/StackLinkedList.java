2f39e8e183a94bfedc588cbe00f1055f83827a49
sivapriya568 <147132673+sivapriya568@users.noreply.github.com>
2023-12-13 20:38:32 +0530

    Add files via upload

---
+++ 22wh1a0568-class/StackLinkedList.java
@@ -0,0 +1,58 @@
+package LearningJava;
+class Node1 {
+    int data;
+    Node1 link;
+
+    Node1(int data) {
+        this.data = data;
+        this.link = null;
+    }
+}
+
+public class StackLinkedList {
+    private static Node1 top = null;
+
+    public static void push(int data) {
+        Node1 newNode = new Node1(data);
+        newNode.link = top;
+        top = newNode;
+    }
+
+    public static int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot pop.");
+            return -1;
+        }
+        Node1 temp = top;
+        int val = temp.data;
+        top = top.link;
+        temp.link = null;
+        return val;
+    }
+
+    public static boolean isEmpty() {
+        return top == null;
+    }
+
+    public static void display() {
+        Node1 temp = top;
+        System.out.print("Stack elements are:");
+        while (temp != null) {
+            System.out.print(" " + temp.data);
+            temp = temp.link;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        push(35);
+        push(78);
+        push(456);
+        push(345);
+        display();
+
+        pop();
+        System.out.println("After deletion ");
+        display();
+    }
+}
