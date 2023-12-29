c703f891864bd9115212d0a1e96f6c74e9cbe3db
22wh1a05a8 <147132110+22wh1a05a8@users.noreply.github.com>
2023-12-29 11:44:38 +0530

    Update CalculatorGui1.java

--- 22wh1a05a8-Lab/CalculatorGui1.java
+++ 22wh1a05a8-Lab/CalculatorGui1.java
@@ -53,9 +53,16 @@ class Calc extends Frame implements ActionListener{
 				num.setText(r2);
 			}
 			if(s.equals("/")) {
-				int res = n1/n2;
-				String r2 = Integer.toString(res);
-				num.setText(r2);
+				int n1 = Integer.parseInt(num1.getText());
+				int n2 = Integer.parseInt(num2.getText());
+				try{
+					int res = n1/n2;
+					String s = Integer.toString(res);
+					r.setText(s);
+				}
+				catch(ArithmeticException ae1){
+					r.setText("Arithmetic Exception");
+				}
 			}
 		}
 		if(str.equals("Clear")) {
