0f44148728ee15405b0ccae8b7ebc4383d201117
23wh5a0512 <147629900+23wh5a0512@users.noreply.github.com>
2023-12-30 21:57:55 +0530

    Add files via upload

---
+++ 23wh5a0512-class/Bankacc.java
@@ -0,0 +1,47 @@
+import  java.util.*;
+class Bank
+{
+double bal;
+Bank(double b)
+{
+bal=b;
+}
+void contact(double r)
+{
+Scanner sc=new Scanner(System.in);
+String pwd;
+System.out.println("enter pwd");
+pwd=sc.next();
+if (pwd.equals("Ashwitha"))
+{
+Interest in=new Interest(r);
+in.calcInter();
+}
+else
+{
+System.out.println("not");
+}
+}
+class Interest
+{
+double rate;
+Interest(double r)
+{
+rate=r;
+}
+void calcInter()
+{
+int t=10;
+double s=(bal*t*rate/100);
+System.out.println("interest"+s);
+}
+}
+}
+class Balance
+{
+public static void  main(String args[])
+{
+Bank b=new Bank(1000);
+b.contact(6);
+}
+}
