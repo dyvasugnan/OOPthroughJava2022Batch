3f20b1129911602e93c25c3cc4d812cd92fada02
Manvithach97 <147131977+Manvithach97@users.noreply.github.com>
2024-01-03 06:04:04 +0530

    Add files via upload

---
+++ 22wh1a0597-class/Main.java
@@ -0,0 +1,52 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insert(5);
+        list.insert(10);
+        list.insert(15);
+        list.insert(20);
+
+        System.out.println("Linked List elements:");
+        list.display();
+    }
+}
+
+
+
